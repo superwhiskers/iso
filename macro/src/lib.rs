c3ce782a92913f3f9a36f3ac3dff341014021310
superwhiskers <whiskerdev@protonmail.com>
2023-01-13 21:05:31 -0600

    iso: normalize license headers

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,10 +1,11 @@
 //
 // iso - implementations of data types related to common iso standards
+// Copyright (c) 2021 superwhiskers <whiskerdev@protonmail.com>
 //
-// copyright (c) 2021 superwhiskers <whiskerdev@protonmail.com>
-// this source code form is subject to the terms of the mozilla public
-// license, v. 2.0. if a copy of the mpl was not distributed with this
-// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //
 
 #![allow(clippy::cognitive_complexity)]
